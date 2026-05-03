44082de278c58de237d33760502cb71ebd5c9477
Saw Thura Zaw <irrssue@gmail.com>
2026-05-03 05:27:33 -0400

    auto: update StoryRow.tsx

--- src/app/StoryRow.tsx
+++ src/app/StoryRow.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { HNItem, HNComment, getAge, getDomain, fetchComments, sanitizeHNHtml } from "./hn";
+import BookmarkButton from "./BookmarkButton";
 
 function CommentNode({ comment, depth }: { comment: HNComment; depth: number }) {
   const [collapsed, setCollapsed] = useState(false);
