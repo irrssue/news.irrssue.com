24dad4b587326ab009d9e85408ff7c0c6ff5caf2
Saw Thura Zaw <irrssue@gmail.com>
2026-05-03 03:21:48 -0400

    auto: update ProfileButton.tsx

--- src/app/ProfileButton.tsx
+++ src/app/ProfileButton.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
-
-type User = { username: string } | null;
+import { useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
 
 function UserIcon() {
   return (
