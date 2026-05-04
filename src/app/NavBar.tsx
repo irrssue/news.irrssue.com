fb6053243e8f58e8fc18cc70bd342eedc6ada570
Saw Thura Zaw <irrssue@gmail.com>
2026-05-04 18:46:35 -0400

    auto: update NavBar.tsx

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
+import ProfileButton from "./ProfileButton";
 
 function getSavedCount(): number {
   try {
